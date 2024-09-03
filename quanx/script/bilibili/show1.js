9067aa4e2a364ea84326e65b2b5999c2849c57fb
1ris_W <ediath462@gmail.com>
2024-09-04 04:52:47 +0800

    test

---
+++ quanx/script/bilibili/show1.js
@@ -0,0 +1,23 @@
+const url = $request.url;
+if (!$response.body) $done({});
+let obj = JSON.parse($response.body);
+console.log("================begin===============")
+if (obj?.data?.card_list) {
+    for (item in obj.data.card_list) {
+        if (item.card_type === "area_entrance_v1") {
+            return
+        }
+        if (item.card_type === "my_idol_v1") {
+            for (i in item.card_data.my_idol_v1) {
+                console.log(i.title)
+            }
+        }
+        if (item.card_type === "small_card_v1") {
+            for (i in item.card_data.small_card_v1) {
+                console.log(i.title)
+            }
+        }
+    }
+}
+
+$done({ body: JSON.stringify(obj) });
