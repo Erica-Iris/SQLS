672a9e5bfb95991c22069424e5bf6986329f99d6
1ris_W <ediath462@gmail.com>
2024-09-04 05:07:41 +0800

    test

--- quanx/script/bilibili/show1.js
+++ quanx/script/bilibili/show1.js
@@ -5,6 +5,7 @@ console.log("\n================begin===============\n")
 if (obj?.data?.card_list) {
     console.log("获取到：" + obj.data.card_list.length + " 条数据\n")
     for (item in obj.data.card_list) {
+        console.log(item.card_type + "\n")
         if (item.card_type === "my_idol_v1") {
             for (i in item.card_data.my_idol_v1.list) {
                 console.log(i.title)
