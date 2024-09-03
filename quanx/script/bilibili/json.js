9a2b712844ce8ece9a94c3254449d9f1242d37aa
1ris_W <ediath462@gmail.com>
2024-09-03 23:57:43 +0800

    修改脚本

--- quanx/script/bilibili/json.js
+++ quanx/script/bilibili/json.js
@@ -2,12 +2,13 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-if (url.includes("/xlive/app-ucenter/v1/elden/get_by_user")) {
-    if (obj?.data?.interaction_list) {
-        for (i in length(obj.data.interaction_list)) {
-            if (i?.note != "预言") {
-                delete obj.data.interaction_list[i];
-            }
-        }
-    }
+
+if (obj?.data?.interaction_list) {
+    // for (i in obj.data.interaction_list) {
+    //     if (i?.note != "预言") {
+    //         delete obj.data.interaction_list[i];
+    //     }
+    // }
+    delete obj.data.interaction_list[0];
+    delete obj.data.interaction_list[2];
 }
