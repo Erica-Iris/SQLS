acdbccf0fb9859fc1fcf7341ed7a3443669e6be4
1ris_W <ediath462@gmail.com>
2024-09-04 01:31:42 +0800

    test

--- quanx/script/bilibili/json.js
+++ quanx/script/bilibili/json.js
@@ -5,8 +5,9 @@ if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
 if (obj?.data?.interaction_list) {
-    console.log(`[${$script}]`, obj.data.interaction_list);
+    console.log(`发现了`, obj.data.interaction_list);
     obj.data.interaction_list = obj.data.interaction_list.filter(item => item.title === "预言");
+    console.log("过滤后", obj.data.interaction_list);
 }
 
 $done({ body: JSON.stringify(obj) });
