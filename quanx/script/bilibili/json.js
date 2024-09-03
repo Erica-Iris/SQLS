a1e28907852fa96a46fbf1a7a748813f13b3d0b3
1ris_W <ediath462@gmail.com>
2024-09-04 01:04:29 +0800

    test

--- quanx/script/bilibili/json.js
+++ quanx/script/bilibili/json.js
@@ -1,17 +1,9 @@
+// 2023-12-11 19:20
+
 const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
+delete obj.data.interaction_list[0];
 
-console.log("234")
-// if (obj?.data?.interaction_list) {
-//     // for (i in obj.data.interaction_list) {
-//     //     if (i?.note != "预言") {
-//     //         delete obj.data.interaction_list[i];
-//     //     }
-//     // }
-//     delete obj.data.interaction_list[0];
-//     delete obj.data.interaction_list[2];
-// }
-delete obj.data.interaction_list;
 $done({ body: JSON.stringify(obj) });
