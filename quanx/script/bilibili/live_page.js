65d125cde357bec7e2d785a68f432f0e15ea610a
1ris_W <ediath462@gmail.com>
2024-09-05 03:13:20 +0800

    test

--- quanx/script/bilibili/live_page.js
+++ quanx/script/bilibili/live_page.js
@@ -3,66 +3,160 @@ const url = $request.url;
 if (!$response.body) $done({});
 let obj = JSON.parse($response.body);
 
-console.log("获取到: " + obj.data.card_list.length + "条数据");
-const qnDayColorMap = {
-    80: "#FF6F61",
-    150: "#FFA07A",
-    250: "#FFD700",
-    10000: "#98FB98"
-};
-const qnColorMap = {
-    80: "#8B0000",
-    150: "#CD5C5C",
-    250: "#DAA520",
-    10000: "#2E8B57"
-};
-const qnTextMap = {
-    80: "流畅",
-    150: "高清",
-    250: "超清",
-    10000: "原画"
-};
-const categoriesToFilter = ["交友", "聊天电台", "崩坏：星穹铁道", "王者荣耀", "守望先锋", "男声电台", "萌宅领域", "唱见电台", "蔚蓝档案"];
+// console.log("获取到: " + obj.data.card_list.length + "条数据");
+// const qnDayColorMap = {
+//     80: "#FF6F61",
+//     150: "#FFA07A",
+//     250: "#FFD700",
+//     10000: "#98FB98"
+// };
+// const qnColorMap = {
+//     80: "#8B0000",
+//     150: "#CD5C5C",
+//     250: "#DAA520",
+//     10000: "#2E8B57"
+// };
+// const qnTextMap = {
+//     80: "流畅",
+//     150: "高清",
+//     250: "超清",
+//     10000: "原画"
+// };
+// const categoriesToFilter = ["交友", "聊天电台", "崩坏：星穹铁道", "王者荣耀", "守望先锋", "男声电台", "萌宅领域", "唱见电台", "蔚蓝档案"];
 
-// 过滤函数
-function filterCardsByCategory(data, categories) {
-    if (!data || !data.data || !data.data.card_list) {
-        return [];
-    }
-    return data.data.card_list.filter(card => {
-        const areaName = card.card_data?.small_card_v1?.area_name;
-        return !categories.includes(areaName);
-    });
-}
-// 进行过滤
-obj.data.card_list = filterCardsByCategory(obj.data, categoriesToFilter);
+// // 过滤函数
+// function filterCardsByCategory(data, categories) {
+//     if (!data || !data.data || !data.data.card_list) {
+//         return [];
+//     }
+//     return data.data.card_list.filter(card => {
+//         const areaName = card.card_data?.small_card_v1?.area_name;
+//         return !categories.includes(areaName);
+//     });
+// }
+// // 进行过滤
+// obj.data.card_list = filterCardsByCategory(obj.data, categoriesToFilter);
 
-obj.data.card_list.forEach(card => {
-    if (card.card_data && card.card_data.small_card_v1) {
-        const smallCard = card.card_data.small_card_v1;
+// obj.data.card_list.forEach(card => {
+//     if (card.card_data && card.card_data.small_card_v1) {
+//         const smallCard = card.card_data.small_card_v1;
 
-        // 获取current_qn的值
-        const currentQn = smallCard.current_qn;
-        // 根据current_qn的值修改text_night_color
-        if (qnColorMap.hasOwnProperty(currentQn)) {
-            // 根据画质修改颜色
-            smallCard.subtitle_style.text_night_color = qnColorMap[currentQn];
-            smallCard.subtitle_style.text_color = qnDayColorMap[currentQn];
-            // 显示画质
-            smallCard.feed_tag.tag_text = qnTextMap[currentQn];
+//         // 获取current_qn的值
+//         const currentQn = smallCard.current_qn;
+//         // 根据current_qn的值修改text_night_color
+//         if (qnColorMap.hasOwnProperty(currentQn)) {
+//             // 根据画质修改颜色
+//             smallCard.subtitle_style.text_night_color = qnColorMap[currentQn];
+//             smallCard.subtitle_style.text_color = qnDayColorMap[currentQn];
+//             // 显示画质
+//             smallCard.feed_tag.tag_text = qnTextMap[currentQn];
+//         }
+//         // 删除没用的埋点
+//         smallCard.show_callback = "";
+//         smallCard.click_callback = "";
+//         // 删除预先获取的默认画质的播放链接
+//         smallCard.play_url = "";
+//         // 删除封面左上和右上的各种百舰等图标
+//         smallCard.pendent_list = [];
+//         // 隐藏反馈按钮
+//         smallCard.is_hide_feedback = 1;
+
+//     }
+// });
+// console.log("剩余: " + obj.data.card_list.length + "条数据");
+
+// $done({ body: JSON.stringify(obj) });
+
+
+async function onResponse(response) {
+    // console.log(response);
+    console.log("===============BEGIN=================")
+
+    // Update or Add new headers
+    // response.headers["Content-Type"] = "application/json";
+
+    // Update status Code
+    // response.statusCode = 500;
+
+    // Update Body
+    // var body = response.body;
+    // body["new-key"] = "Proxyman";
+
+    var body = response.body;
+    //   console.log("获取到: "+body.data.card_list.length+"条数据");
+    const qnDayColorMap = {
+        80: "#FF6F61",
+        150: "#FFA07A",
+        250: "#FFD700",
+        10000: "#98FB98"
+    };
+    const qnColorMap = {
+        80: "#8B0000",
+        150: "#CD5C5C",
+        250: "#DAA520",
+        10000: "#2E8B57"
+    };
+    const qnTextMap = {
+        80: "流畅",
+        150: "高清",
+        250: "超清",
+        10000: "原画"
+    };
+    const categoriesToFilter = ["交友", "聊天电台", "崩坏：星穹铁道", "王者荣耀", "守望先锋", "男声电台", "萌宅领域", "唱见电台", "蔚蓝档案"];
+    const data = response.body;
+
+    // 过滤函数
+    function filterCardsByCategory(data, categories) {
+        if (!data || !data.data || !data.data.card_list) {
+            return [];
         }
-        // 删除没用的埋点
-        smallCard.show_callback = "";
-        smallCard.click_callback = "";
-        // 删除预先获取的默认画质的播放链接
-        smallCard.play_url = "";
-        // 删除封面左上和右上的各种百舰等图标
-        smallCard.pendent_list = [];
-        // 隐藏反馈按钮
-        smallCard.is_hide_feedback = 1;
 
+        return data.data.card_list.filter(card => {
+            const areaName = card.card_data?.small_card_v1?.area_name;
+            return !categories.includes(areaName);
+        });
     }
-});
-console.log("剩余: " + obj.data.card_list.length + "条数据");
+    // 进行过滤
+    body.data.card_list = filterCardsByCategory(data, categoriesToFilter);
 
-$done({ body: JSON.stringify(obj) });
+    body.data.card_list.forEach(card => {
+        if (card.card_data && card.card_data.small_card_v1) {
+            const smallCard = card.card_data.small_card_v1;
+
+            // 获取current_qn的值
+            const currentQn = smallCard.current_qn;
+            // 根据current_qn的值修改text_night_color
+            if (qnColorMap.hasOwnProperty(currentQn)) {
+                // 根据画质修改颜色
+                smallCard.subtitle_style.text_night_color = qnColorMap[currentQn];
+                smallCard.subtitle_style.text_color = qnDayColorMap[currentQn];
+                // 显示画质
+                smallCard.feed_tag.tag_text = qnTextMap[currentQn];
+            }
+            // 删除没用的埋点
+            smallCard.show_callback = "";
+            smallCard.click_callback = "";
+            // 删除预先获取的默认画质的播放链接
+            smallCard.play_url = "";
+            // 删除封面左上和右上的各种百舰等图标
+            smallCard.pendent_list = [];
+            // 隐藏反馈按钮
+            smallCard.is_hide_feedback = 1;
+
+        }
+    });
+    console.log("剩余: " + body.data.card_list.length + "条数据");
+    response.body = body;
+
+
+
+
+    // Or map a local file as a body
+    //     response.bodyFilePath = "/Users/iris/Documents/Project/SQLS/quanx/script/bilibili/a.json"
+    console.log("================END==================")
+    // Done
+    return response;
+}
+
+
+$done(onResponse($response));
